fad4bae7e93f370e747bdaf6060d7e43975025a5
Rahul Sandhu <rahul@sandhuservices.dev>
2022-11-25 04:54:26 +0000

    Fixed first run again again again

--- implementation-notes.md
+++ implementation-notes.md
@@ -1,3 +1,7 @@
 I put phone number as int, which will remove the 0 from certain phone numbers
 
 I used 'or' in an if statement, which cause the program to exit after any user input is given, depsite the condition. This was fixed by using 'if var in'
+
+I created the table every time, which caused an error when it already existed, so I decided to create a first_run function that checks for a database file, and checks whether it is empty.
+
+This caused the program to crash if a directory called allotment.db is created, as the program will interpret it as a database file and attempt to make a table. I solved this by 
--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 import sqlite3
-from pathlib import Path
 
 ### Functions
+
 ## First run
 def first_run():
-    db_path = Path('allotment-manager/allotment.db')
-    db_path_exists = db_path.is_file()
-    try:
-        db_path_size = db_path.stat().st_size
-    except:
-        db_path_size = None
     db_connection = sqlite3.connect('allotment-manager/allotment.db')
-    if not db_path_exists  or db_path_size == 0:
-        # Create the 'USERS' table with the fields NAME, ADDRESS, PHONENUMBER, and PLANTID
-        db_connection.execute('''CREATE TABLE USERS
-            (ID INT PRIMARY KEY NOT NULL,
-            NAME            TEXT NOT NULL,
-            ADDRESS         CHAR(50),
-            PHONENUMBER     INT,
-            PLANTID         );''')
+    # Create the 'USERS' table with the fields NAME, ADDRESS, PHONENUMBER, and PLANTID
+    db_connection.execute('''CREATE TABLE IF NOT EXISTS USERS
+        (ID INT PRIMARY KEY NOT NULL,
+        NAME            TEXT NOT NULL,
+        ADDRESS         CHAR(50),
+        PHONENUMBER     INT,
+        PLANTID         );''')
+    print("Welcome to Allotment Manager 1.0! Type \'help\' for help.")
 
-        print("TABLE 'USERS' created.")    
-        print("Welcome to Allotment Manager 1.0! Type \'help\' for help.")
-    else:
-        print("Welcome to Allotment Manager 1.0! Type \'help\' for help.")
 ## Creating functions for Prompt
 # Exit function
 def prompt_exit():
