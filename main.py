cc0b2f51a07edcd42f9baa62f68efd693b6828a6
Rahul Sandhu <rahul@sandhuservices.dev>
2022-11-25 03:51:53 +0000

    Added first run and help method

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 import sqlite3
+from pathlib import Path
 
-db_connection = sqlite3.connect('allotment.db')
-
-db_connection.execute('''CREATE TABLE USERS
+### Functions
+## First run
+def first_run(db_connection):
+    db_path = Path('allotment.db')
+    if db_path.is_file() == False:
+        # Create the 'USERS' table with the field NAME, ADDRESS, PHONENUMBER, and PLANTID
+        db_connection.execute('''CREATE TABLE USERS
             (ID INT PRIMARY KEY NOT NULL,
             NAME            TEXT NOT NULL,
             ADDRESS         CHAR(50),
             PHONENUMBER     INT,
             PLANTID         );''')
-
-db_connection.close()
-
-while True:
-    user_exec = input("Prompt/> ")
-    if user_exec.lower() in ['quit', 'exit']:
-        print("Goodbye!")
-        exit()
     else:
         pass
+## Creating functions for Prompt
+# Exit function
+def prompt_exit():
+    print("Goodbye!")
+    exit()
+# Prompt Function
+def prompt_help():
+    print("Supported Commands:")
+    print("- exit: exits the program\n- quit: same as exit\n- help: calls this help method")
+
+
+# Create the database connection
+db_connection = sqlite3.connect('allotment-manager/allotment.db')
+
+
+while True:
+    # Generate the prompt
+    user_exec = input("Prompt/> ")
+
+    # Parse the user's input
+    if user_exec.lower() in ['quit', 'exit']: # Exit the prompt
+       prompt_exit() 
+    elif user_exec.lower() in ['help']: # Call the help method
+        prompt_help()
+    else:
+        pass
+
+db_connection.close()
